770e57a8c55080001268cdfd8a75963c1ad75723
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-08 11:16:02 +0300

    Added actions and outlets

--- Voice Tweak/ViewController.swift
+++ Voice Tweak/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var speedLabel  : UILabel!
+    @IBOutlet weak var speedSlider : UISlider!
+    @IBOutlet weak var loopSwitch  : UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func recordTapped(sender: UIButton) {
+        
     }
-
-
+    
+    @IBAction func playTapped(sender: UIButton) {
+        
+    }
 }
-
