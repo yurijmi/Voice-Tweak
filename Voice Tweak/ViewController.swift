ea8561d583b24040d471a2f78e0bd1d08724e4e2
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-08 11:43:58 +0300

    Playing sound

--- Voice Tweak/ViewController.swift
+++ Voice Tweak/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     @IBOutlet weak var speedLabel  : UILabel!
+    @IBOutlet weak var playButton  : UIButton!
     @IBOutlet weak var loopSwitch  : UISwitch!
+    
+    var audioPlayer : AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +32,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func playTapped(sender: UIButton) {
-        
+        if self.audioPlayer == nil {
+            setupAndPlay()
+        } else {
+            if self.audioPlayer!.playing {
+                self.audioPlayer!.stop()
+                
+                self.playButton.setTitle("PLAY", forState: UIControlState.Normal)
+            } else {
+                setupAndPlay()
+            }
+        }
     }
+    
+    func setupAndPlay() {
+        do {
+            let path = NSBundle.mainBundle().pathForResource("mic_noise", ofType: "m4a")
+            let url  = NSURL(fileURLWithPath: path!)
+            
+            self.audioPlayer = try AVAudioPlayer(contentsOfURL: url)
+            self.audioPlayer!.delegate = self
+            self.audioPlayer!.play()
+            
+            self.playButton.setTitle("STOP", forState: UIControlState.Normal)
+        } catch {}
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        self.playButton.setTitle("PLAY", forState: UIControlState.Normal)
+    }
+    
 }
